51e8a9a1384294c532efa714559100029157eeb0
guod19 <jg4050@columbia.edu>
2018-07-10 16:46:34 -0400

    .

--- src/main/java/WC/WorldCupMap.java
+++ src/main/java/WC/WorldCupMap.java
@@ -7,8 +7,24 @@ public class WorldCupMap {
 
     public Map<String, String> createGroupPhaseMap() {
         Map<String, String> groupPhase = new HashMap<>();
-        groupPhase.put("RUSxKSA", "5x0");
+        groupPhase.put("RUS x KSA", "5-0");
 
         return groupPhase;
     }
+
+    public Map<String, String> createSixteenPhaseMap() {
+        Map<String, String> sixteenPhase = new HashMap<>();
+        sixteenPhase.put("FRA x ARG", "4-3");
+        sixteenPhase.put("ESP x RUS", "4-5");
+        sixteenPhase.put("BRA x MEX", "2-0");
+        sixteenPhase.put("SWE x SUI", "1-0");
+        sixteenPhase.put("URU x POR", "2-1");
+        sixteenPhase.put("CRO x DEN", "4-3");
+        sixteenPhase.put("BEL x JPN", "3-2");
+        sixteenPhase.put("COL x ENG", "4-5");
+
+
+        return sixteenPhase;
+    }
+
 }
