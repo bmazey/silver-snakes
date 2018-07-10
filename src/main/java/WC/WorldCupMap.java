dc59b6d046994f5d8b7a77ad6e36e54d6bb7da63
ahmad <ahmad.badghaish1@gmail.com>
2018-07-10 16:14:58 -0400

    Group phase almost done

--- src/main/java/WC/WorldCupMap.java
+++ src/main/java/WC/WorldCupMap.java
@@ -7,8 +7,18 @@ public class WorldCupMap {
 
     public Map<String, String> createGroupPhaseMap() {
         Map<String, String> groupPhase = new HashMap<>();
-        groupPhase.put("RUSxKSA", "5x0");
-
+        groupPhase.put("RUS x KSA", "5-0");
+        groupPhase.put("EGY x URU", "0-1");
+        groupPhase.put("MAR x IRN", "0-1");
+        groupPhase.put("POR x ESP", "3-3");
+        groupPhase.put("FRA x AUS", "2-1");
+        groupPhase.put("ARG x ISL", "1-1");
+        groupPhase.put("PER x DEN", "0-1");
+        groupPhase.put("CRO x NGA", "2-0");
+        groupPhase.put("CRC x SRB", "0-1");
+        groupPhase.put("GER x MEX", "0-1");
+        groupPhase.put("BRA x SUI", "1-1");
+        groupPhase.put("")
         return groupPhase;
     }
 }
