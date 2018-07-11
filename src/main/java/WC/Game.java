af7e835de974ebcee24aa6b41a8cb58f68345ca7
ninahutzler <mh3899@columbia.edu>
2018-07-11 15:16:26 -0400

    It all worked

--- src/main/java/WC/Game.java
+++ src/main/java/WC/Game.java
@@ -23,29 +23,39 @@ public class Game {
 */
         Random       random    = new Random();
 
-        List<String> keys1      = new ArrayList<String>(groupPhase.keySet());
-        String       randomKey1 = keys1.get( random.nextInt(keys1.size()) );
-        String       value1     = groupPhase.get(randomKey1);
-        System.out.println("Match : " + randomKey1 + " , result : ");
-        Scanner input1 = new Scanner(System.in);
-        String guess1 = input1.nextLine();
-
-        List<String> keys2      = new ArrayList<String>(Sixteenth.keySet());
-        String       randomKey2 = keys2.get( random.nextInt(keys2.size()) );
-        String       value2     = groupPhase.get(randomKey2);
-        System.out.println("Match : " + randomKey2 + " , result : ");
-        Scanner input2 = new Scanner(System.in);
-        String guess2 = input2.nextLine();
-
         List<String> keys3      = new ArrayList<String>(Quarters.keySet());
         String       randomKey3 = keys3.get( random.nextInt(keys3.size()) );
-        String       value3     = groupPhase.get(randomKey3);
+        String       value3     = Quarters.get(randomKey3);
         System.out.println("Match : " + randomKey3 + " , result : ");
         Scanner input3 = new Scanner(System.in);
         String guess3 = input3.nextLine();
 
-        if (guess1.equals(value1)){
-            System.out.println("Congrats, you got it!");
+        if (guess3.equals(value3)){
+            System.out.println("Congrats, you got the first level!");
+
+            List<String> keys2      = new ArrayList<String>(Sixteenth.keySet());
+            String       randomKey2 = keys2.get( random.nextInt(keys2.size()) );
+            String       value2     = Sixteenth.get(randomKey2);
+            System.out.println("Match : " + randomKey2 + " , result : ");
+            Scanner input2 = new Scanner(System.in);
+            String guess2 = input2.nextLine();
+
+            if (guess2.equals(value2)) {
+                System.out.println("Congrats, you got the second level!");
+
+                List<String> keys1      = new ArrayList<String>(groupPhase.keySet());
+                String       randomKey1 = keys1.get( random.nextInt(keys1.size()) );
+                String       value1     = groupPhase.get(randomKey1);
+                System.out.println("Match : " + randomKey1 + " , result : ");
+                Scanner input1 = new Scanner(System.in);
+                String guess1 = input1.nextLine();
+
+                if (guess1.equals(value1)){
+                    System.out.println("Congrats, you got it all!");
+                }
+                else { System.out.println("Sorry, wrong answer.");}
+            }
+            else { System.out.println("Sorry, wrong answer.");}
         }
         else { System.out.println("Sorry, wrong answer.");}
     }
