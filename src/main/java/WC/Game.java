0f75c6703cfa5688047d1b062781d97d9ae3d46e
ahmad <ahmad.badghaish1@gmail.com>
2018-07-11 12:39:55 -0400

    Improved the game (now it tells you the corrct result if you answered wrong + I made the loop!)

--- src/main/java/WC/Game.java
+++ src/main/java/WC/Game.java
@@ -14,26 +14,31 @@ public class Game {
     static Map<String, String> groupPhase = myWCMap.createGroupPhaseMap();
     // private static String guess;
 
-    public static void main (String[] args){
+    public static void main (String[] args) {
       /* Random rand = new Random();
        int resultToGuess = rand.nextInt(4);
        Scanner input = new Scanner(System.in);
        System.out.println(Quarters.get(resultToGuess));
        guess = input.next();
 */
-
-        Random       random    = new Random();
-        List<String> keys      = new ArrayList<String>(groupPhase.keySet());
-        String       randomKey = keys.get( random.nextInt(keys.size()) );
-        String       value     = groupPhase.get(randomKey);
+        for (int i = 0; i < 1;) {
+        Random random = new Random();
+        List<String> keys = new ArrayList<String>(groupPhase.keySet());
+        String randomKey = keys.get(random.nextInt(keys.size()));
+        String value = groupPhase.get(randomKey);
         System.out.print("Match : " + randomKey + ",\nresult : ");
         Scanner input = new Scanner(System.in);
         String guess = input.next();
 
-        if (guess.equals(value)){
-            System.out.println("\nCongrats, you got it!");
-        }
-        else { System.out.println("\nSorry, wrong answer. \nThe result is : " + value + " .");}
-    }
 
+
+
+            if (guess.equals(value)) {
+                System.out.println("\nCongrats, you got it!");
+            } else {
+                System.out.println("\nSorry, wrong answer. \nThe result is : " + value + " .");
+                i ++;
+            }
+        }
+    }
 }
