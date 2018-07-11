498b04901004bfae8be689d629be00963e304851
ninahutzler <mh3899@columbia.edu>
2018-07-11 13:00:44 -0400

    First part worked

--- src/main/java/WC/Game.java
+++ src/main/java/WC/Game.java
@@ -21,16 +21,30 @@ public class Game {
        System.out.println(Quarters.get(resultToGuess));
        guess = input.next();
 */
-
         Random       random    = new Random();
-        List<String> keys      = new ArrayList<String>(groupPhase.keySet());
-        String       randomKey = keys.get( random.nextInt(keys.size()) );
-        String       value     = groupPhase.get(randomKey);
-        System.out.println("Match : " + randomKey + " , result : ");
-        Scanner input = new Scanner(System.in);
-        String guess = input.nextLine();
 
-        if (guess.equals(value)){
+        List<String> keys1      = new ArrayList<String>(groupPhase.keySet());
+        String       randomKey1 = keys1.get( random.nextInt(keys1.size()) );
+        String       value1     = groupPhase.get(randomKey1);
+        System.out.println("Match : " + randomKey1 + " , result : ");
+        Scanner input1 = new Scanner(System.in);
+        String guess1 = input1.nextLine();
+
+        List<String> keys2      = new ArrayList<String>(Sixteenth.keySet());
+        String       randomKey2 = keys2.get( random.nextInt(keys2.size()) );
+        String       value2     = groupPhase.get(randomKey2);
+        System.out.println("Match : " + randomKey2 + " , result : ");
+        Scanner input2 = new Scanner(System.in);
+        String guess2 = input2.nextLine();
+
+        List<String> keys3      = new ArrayList<String>(Quarters.keySet());
+        String       randomKey3 = keys3.get( random.nextInt(keys3.size()) );
+        String       value3     = groupPhase.get(randomKey3);
+        System.out.println("Match : " + randomKey3 + " , result : ");
+        Scanner input3 = new Scanner(System.in);
+        String guess3 = input3.nextLine();
+
+        if (guess1.equals(value1)){
             System.out.println("Congrats, you got it!");
         }
         else { System.out.println("Sorry, wrong answer.");}
